7e9d0e3c3c699bf750fb3f0a208740a93b621457
Kristian Kielhofner <kris@tovera.com>
2023-09-01 08:36:29 -0500

    Add utils.sh

---
+++ utils.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -r venv/bin/activate ]; then
+    source venv/bin/activate
+else
+    echo "You need to create venv - exiting"
+    exit 1
+fi
+
+dev() {
+    mkdocs serve -a 0.0.0.0:8001
+}
+
+case $1 in
+
+dev)
+    dev
+;;
+
+*)
+    echo "Unsupported option"
+;;
+
+esac
